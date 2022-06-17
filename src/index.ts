7e2fd41f62747def1381f02bf02e84e23d33342c
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2022-06-17 18:58:01 -0300

    add express helmet

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import cors from 'cors';
 import express, { Express, Request, Response } from 'express';
+import helmet from 'helmet';
 
 const app: Express = express();
 const port = 3000;
 
 app.use(cors());
+app.use(helmet());
 
 app.get('/', (req: Request, res: Response) => {
   res.json({message: 'Hello World'});
