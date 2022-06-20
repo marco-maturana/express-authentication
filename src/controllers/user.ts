10a36778fcc5ef03ceb6a0e10c933eb14dbe8554
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2022-06-20 16:19:55 -0300

    user signIn endpoint

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,6 +2,22 @@ import * as authentication from '../services/authentication';
 import {Request, Response} from 'express';
 import UserModel from '../models/user';
 
+export async function signIn(req: Request, res: Response): Promise<Response> {
+  const {email, password} = req.body;
+
+  const user = await authentication.signIn({email, password});
+
+  if (user == null) return res.status(401).json({error: 'Usuario ou senha inválida!'});
+
+  const token = await authentication.generateToken(user);
+
+  return res.json({
+    name: user.name,
+    email: user.email,
+    token,
+  })
+}
+
 export async function signUp(req: Request, res: Response): Promise<void> {
   const {body} = req;
 
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,11 @@
 /* eslint-disable import/extensions */
 import express from 'express';
 
-import { signUp } from './controllers/user';
+import { signUp, signIn } from './controllers/user';
 
 const router = express.Router();
 
+router.post('/sign-in', async (req, res) => signIn(req, res));
 router.post('/sign-up', async (req, res) => signUp(req, res));
 
 export default router;
