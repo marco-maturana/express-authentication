92b55d2de014c90b9eaf33dedfaae56fb7040414
Marco Aurelio Maturana Silveira <marco.maturana.ro@gmail.com>
2022-06-20 16:07:37 -0300

    authentication service

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,11 +1,11 @@
-import * as argon2 from "argon2";
+import * as authentication from '../services/authentication';
 import {Request, Response} from 'express';
 import UserModel from '../models/user';
 
 export async function signUp(req: Request, res: Response): Promise<void> {
   const {body} = req;
 
-  const password = await argon2.hash(body.password);
+  const password = await authentication.encryptPassword(body.password);
 
   await UserModel.create({...body, password});
 
---
+++ src/services/authentication.ts
@@ -0,0 +1,34 @@
+import * as argon2 from "argon2";
+import jwt from 'jsonwebtoken';
+import UserModel, {User} from '../models/user';
+
+export interface SignInArgs {
+  email: string;
+  password: string;
+}
+
+export async function encryptPassword(password: string): Promise<string> {
+  return argon2.hash(password);
+}
+
+export function generateToken(user: User): string {
+  if (process.env.JWT_SECRET == null) throw new Error('JWT_SECRET needs to be defined!');
+
+  const {name, email, _id} = user;
+
+  return jwt.sign({name, email}, process.env.JWT_SECRET, {
+    subject: _id.toHexString(),
+  });
+}
+
+export async function signIn({email, password}: Readonly<SignInArgs>): Promise<User | null> {
+  const user = await UserModel.findOne({email}).lean();
+
+  if (user != null) {
+    if (await argon2.verify(user.password, password)) {
+      return user;
+    }
+  }
+
+  return null;
+}
